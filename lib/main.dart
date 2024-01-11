82065a2aaa3740e65b5a0448d38d498f02d1000c
sieuhuflit <sieuhuflit@gmail.com>
2024-01-11 11:32:20 +0700

    fix: bug 3

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Breaking change 2 now',
+      title: 'Bug 3',
       theme: ThemeData(
         // This is the theme of your application.
         //
