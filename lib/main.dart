9ff26ba405c737aa77f3698342d5288791b6f4ff
sieuhuflit <sieuhuflit@gmail.com>
2024-01-11 11:39:11 +0700

    feat: feat 6

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Breaking change 5',
+      title: 'Feature 6',
       theme: ThemeData(
         // This is the theme of your application.
         //
