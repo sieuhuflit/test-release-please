216c3afd5065fbc00092b464d456de93dc75a613
Sieu Thai <sieuhuflit@gmail.com>
2024-01-11 11:26:01 +0700

    feat: feature 2 (#3)
    * feat!: refactor main file
    
    * chore: update readme
    
    * refactor: readme file
    
    * ci: add ci cd script

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Feature 1',
+      title: 'Breaking change 2 now',
       theme: ThemeData(
         // This is the theme of your application.
         //
